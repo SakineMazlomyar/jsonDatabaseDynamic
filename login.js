65df39601c4d94fb31d5a65d98eb070a8f01b89b
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-22 00:11:10 +0100

    getByAjax

---
+++ login.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $("#ajax").on("submit", function(event){
+        //event.preventDefault()
+        console.log("sakine")
+
+        /* 
+        function ajax-button(){
+
+        $.ajax({
+
+            type: 'post',
+            url: 'login.php',
+            data: $(this).serialize(),
+            success: function(data){
+                console.log(data);
+               
+            }
+
+        });
+        } */
+    })
+})
