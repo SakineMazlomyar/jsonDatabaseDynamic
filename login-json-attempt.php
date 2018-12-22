ae5ddcdf6d9a3a4e74305d34db42a7fe93267f8d
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-22 19:18:25 +0100

    added

--- login-json-attempt.php
+++ login-json-attempt.php
@@ -1,2 +1,23 @@
 <?php
+
+
+$databasJson = file_get_contents("database.json");
+$databas = json_decode($databasJson);
+
+
+//error_log() for log errors
+foreach($databas->register as $u){
+    
+            if($u->password == $_POST['password']){
+                $s =  $_POST['password'];
+                //$m = $mail;
+                echo "you are signed in as: ".$s; //." ".  $m;
+                return $s; //. $m;
+            }
+}
+
+    echo " you are not registered yet"
+
 ?>
+
+
