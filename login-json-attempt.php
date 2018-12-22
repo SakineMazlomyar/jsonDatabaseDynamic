bd609dda9f4796915aff59a35aad840dcb9099ea
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-22 19:40:33 +0100

    finished

--- login-json-attempt.php
+++ login-json-attempt.php
@@ -1,22 +1,41 @@
 <?php
+    $json = array(
+        "success"=> false,
+        "password"=> "",
+        "mail"=> "",
+        "message"=>""
 
+    );
+    $loginIsClicked = isset($_POST["ajaxButton"]);
+    if($loginIsClicked){
 
-$databasJson = file_get_contents("database.json");
-$databas = json_decode($databasJson);
+        $mail = $_POST["mail"];
+        $password = $_POST["password"];
+
+    }
+    $databasJson = file_get_contents("database.json");
+    $databas = json_decode($databasJson);
 
 
 //error_log() for log errors
-foreach($databas->register as $u){
-    
-            if($u->password == $_POST['password']){
-                $s =  $_POST['password'];
-                //$m = $mail;
-                echo "you are signed in as: ".$s; //." ".  $m;
-                return $s; //. $m;
-            }
-}
-
-    echo " you are not registered yet"
+    foreach($databas->register as $user){
+        
+                if($user->password == $password){
+                    $json["password"]=  $password;
+                    $json["mail"] = $mail;
+                    $json["success"] = true;
+                    //echo "you are signed in as: ".$s ." ".  $m;
+                    header("Content-Type: application/json");
+                    echo json_encode($json);
+                    return;
+                }
+    }
+        $message = " you are not registered yet";
+        $json["message"] = $message;
+        header("Content-Type: application/json");
+        echo json_encode($json);
+      
+        
 
 ?>
 
